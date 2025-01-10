9e96b2146104c1bb52571c682a7afef6bf5f1d68
cielaton <vuwin24680@gmail.com>
2025-01-10 17:13:02 +0700

    [Server] Configured the zerolog and added server's recover middleware

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"net/http"
@@ -11,10 +12,25 @@ import (
 )
 
 func main() {
+	// Init echo instance
+	echoServer := echo.New()
+	// Make the server recover on panic
+	echoServer.Use(middleware.Recover())
+
 	// Logger init with pretty format and timestamp enabled
 	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}).With().Timestamp().Logger()
 	// Set the logger to use globally
 	log.Logger = logger
+	// Logger configuration which enable necessary fields
+	echoServer.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogURI:      true,
+		LogStatus:   true,
+		LogRemoteIP: true,
+		LogValuesFunc: func(echoContext echo.Context, loggerValues middleware.RequestLoggerValues) error {
+			logger.Info().Str("Address", loggerValues.RemoteIP).Str("URI", loggerValues.URI).Int("Status", loggerValues.Status).Msg("Request")
+			return nil
+		},
+	}))
 
 	// Load environment variables from .env file
 	projectRootPath, err := os.Getwd()
@@ -26,8 +42,6 @@ func main() {
 		log.Fatal().Err(err).Msg("[Server] Error loading .env file")
 	}
 
-	// Init echo instance
-	echoServer := echo.New()
 	echoServer.GET("/", func(echoContext echo.Context) error {
 		return echoContext.String(http.StatusOK, "Hello, World!")
 	})
