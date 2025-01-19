055e2b55e326f8311e72f1ebe01cc9be780944bf
cielaton <vuwin24680@gmail.com>
2025-01-19 12:57:19 +0700

    [Location] Fixed the location query error to return the latest published location from multiple devices

--- database/location/get.go
+++ database/location/get.go
@@ -14,23 +14,31 @@ func GetLocation(deviceIds []string, database *mongo.Client) ([]model.Location, 
 	// Get the collection
 	collection := database.Database("Beach-Rescue-Assistance-System").Collection("Location")
 
-	// Get the location
-	// Specify the query field
-	filter := bson.D{{"deviceId", bson.M{"$in": deviceIds}}}
-	// Specify the query option
-	opts := options.Find().SetSort(bson.D{{"datePublished", -1}}).SetLimit(1)
-	// Query the location
-	cursor, err := collection.Find(context.Background(), filter, opts)
-	if err != nil {
-		log.Err(err).Msg("[Database] Failed to query the locations")
-		return []model.Location{}, err
-	}
+	// Iterate through the device ids
+	for _, deviceId := range deviceIds {
+		// Using the array since cursor decode requires an array as parameter
+		var tempLocation []model.Location
+		// Get the location
+		// Specify the query field
+		filter := bson.D{{"deviceId", deviceId}}
+		// Specify the query option
+		opts := options.Find().SetSort(bson.D{{"datePublished", -1}}).SetLimit(1)
+		// Query the location
+		cursor, err := collection.Find(context.Background(), filter, opts)
+		if err != nil {
+			log.Err(err).Msg("[Database] Failed to query the locations")
+			return []model.Location{}, err
+		}
 
-	// decodes the query result
-	err = cursor.All(context.Background(), &locations)
-	if err != nil {
-		log.Err(err).Msg("[Database] Failed to decode locations")
-		return []model.Location{}, err
+		// decodes the query result
+		err = cursor.All(context.Background(), &tempLocation)
+		if err != nil {
+			log.Err(err).Msg("[Database] Failed to decode location")
+			return []model.Location{}, err
+		}
+
+		// Append the location into the location array
+		locations = append(locations, tempLocation[0])
 	}
 
 	return locations, nil
